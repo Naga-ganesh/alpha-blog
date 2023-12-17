ea03f0decf8fe4678f10fee421d1d0bb12b203eb
Naga-ganesh <nagaganesh3108@gmail.com>
2023-12-18 00:02:37 +0530

    add password digest migration for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_17_072100) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_182903) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "uploaded_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -23,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_17_072100) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
