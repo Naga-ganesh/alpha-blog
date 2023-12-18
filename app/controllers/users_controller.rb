178324385507190b7bb456004fb71817b67a9410
Naga-ganesh <nagaganesh3108@gmail.com>
2023-12-18 22:54:50 +0530

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,12 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
